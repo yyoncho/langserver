f22153c69c3db21bca7564b95d1c2e678ba44a81
Ivan Yonchovski <yyoncho@gmail.com>
2022-01-27 20:07:45 +0200

    WIP

--- tests/test_suggestapi.nim
+++ tests/test_suggestapi.nim
@@ -1,36 +1,41 @@
 import
-  unittest,
-  os,
-  std/osproc,
   ../suggestapi,
   faststreams/async_backend,
   faststreams/textio,
   faststreams/inputs,
   faststreams/outputs,
-  faststreams/asynctools_adapters
-
-# let process = startProcess(command = "/home/yyoncho/Sources/nim/langserver/tests/projects/hw/hw.nim",
-#                            workingDir = getCurrentDir(),
-#                            options = {poUsePath, poEvalCommand})
-
-# let input = asyncPipeInput(process.outputHandle)
-
-
-# proc foo(input: AsyncInputStream): Future[void] {.async} =
-#   while async.readable:
-#     let line = input.readLine();
-#     echo ">>>>>>> ", line
+  faststreams/asynctools_adapters,
+  strutils,
+  os,
+  asyncdispatch,
+  asynctools/asyncproc,
+  faststreams/asynctools_adapters,
+  faststreams/textio
 
 
-# waitFor foo()
+let process = startProcess(# command = "cat /home/yyoncho/Sources/nim/langserver/tests/projects/hw/hw.nim",
+                           command = "/home/yyoncho/.nimble/bin/nimsuggest --stdin --find /home/yyoncho/Sources/nim/langserver/tests/projects/hw/hw.nim",
+                           # command = "/home/yyoncho/.nimble/bin//nimlsp",
+                           workingDir = getCurrentDir(),
+                           options = {poUsePath, poEvalCommand})
 
-var process = try:
-  startProcess(command = "cat" & " " & "/home/yyoncho/Sources/nim/jsonrpc/test/fixtures/message.txt",
-               workingDir = getCurrentDir(),
-               options = {poUsePath, poEvalCommand})
-except CatchableError as err:
-  echo "Failed to start process"
-  echo err.msg
-  quit 1
+let input = asyncPipeInput(process.outputHandle)
 
-let b = asyncPipeInput(process.outputHandle)
+let output = asyncPipeOutput(process.inputHandle, allowWaitFor = true)
+
+proc send(input: AsyncOutputStream): Future[void] {.async} =
+  write(OutputStream(output), "def /home/yyoncho/Sources/nim/langserver/tests/projects/hw/hw.nim:2:0\n")
+  flush(output)
+
+
+proc processOutput(input: AsyncInputStream): Future[void] {.async} =
+  while input.readable:
+    let line = await input.readLine();
+    echo ">>>>>>> ", line
+  echo "Done........."
+
+
+waitFor send(output)
+waitFor processOutput(input)
+
+# waitFor processOutput(input)
